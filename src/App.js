756b3550f3026f6ab2c07ea6b83c33fad533df18
Victor Sanchez <v08311996@gmail.com>
2020-01-21 18:09:17 -0500

    Ready for development

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-/*test*/ /*test*/
+/*test*/ /*test ready for development*/
 function App() {
   return (
     <div className="App">
